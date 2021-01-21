141ba87f930c2f4198be35bd13b3f34a43645648
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-01-21 10:15:22 +0200

    Copy analytics.MachineID from k0s

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/k0sproject/k0sctl/analytics"
 	"github.com/k0sproject/k0sctl/version"
 	"github.com/urfave/cli/v2"
 )
@@ -10,9 +11,22 @@ import (
 var versionCommand = &cli.Command{
 	Name:  "version",
 	Usage: "Output k0sctl version",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:   "machine-id",
+			Hidden: true,
+		},
+	},
 	Action: func(ctx *cli.Context) error {
 		fmt.Printf("version: %s\n", version.Version)
 		fmt.Printf("commit: %s\n", version.GitCommit)
+		if ctx.Bool("machine-id") {
+			id, err := analytics.MachineID()
+			if err != nil {
+				id = "failed: " + err.Error()
+			}
+			fmt.Printf("machine-id: %s\n", id)
+		}
 		return nil
 	},
 }
