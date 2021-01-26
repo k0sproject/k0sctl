d16ef9253fbcf7da6de6e025696112fb71759be0
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-01-26 09:49:18 +0200

    Clear todo

--- config/cluster/spec.go
+++ config/cluster/spec.go
@@ -25,20 +25,21 @@ func (s *Spec) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // K0sLeader returns a controller host that is selected to be a "leader",
 // or an initial node, a node that creates join tokens for other controllers.
 func (s *Spec) K0sLeader() *Host {
-	controllers := s.Hosts.Controllers()
-
 	if s.k0sLeader == nil {
+		controllers := s.Hosts.Controllers()
+
 		// Pick the first server that reports to be running and persist the choice
 		for _, h := range controllers {
-			if h.Metadata.K0sBinaryVersion != "" { // TODO && h.InitSystem.ServiceIsRunning("k0s") {
+			if h.Metadata.K0sBinaryVersion != "" && h.Metadata.K0sRunningVersion != "" {
 				s.k0sLeader = h
+				break
 			}
 		}
-	}
 
-	// Still nil?  Fall back to first "server" host, do not persist selection.
-	if s.k0sLeader == nil {
-		return controllers.First()
+		// Still nil?  Fall back to first "server" host, do not persist selection.
+		if s.k0sLeader == nil {
+			return controllers.First()
+		}
 	}
 
 	return s.k0sLeader
