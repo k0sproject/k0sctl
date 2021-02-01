51e285bdb91c0fe9dc30dba9303ed77c6bd613af
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-02-01 15:03:32 +0200

    Improve test

--- config/cluster/mapping_test.go
+++ config/cluster/mapping_test.go
@@ -28,8 +28,8 @@ func TestDigMapping(t *testing.T) {
 
 	assert.Equal(t, "foobar", m.DigMapping("foo")["bar"])
 
-	m.DigMapping("bar", "baz")["dog"] = 1
-	assert.Equal(t, 1, m.Dig("bar", "baz", "dog"))
+	m.DigMapping("foo", "baz")["dog"] = 1
+	assert.Equal(t, 1, m.Dig("foo", "baz", "dog"))
 	// Make sure foo.bar was left intact
 	assert.Equal(t, "foobar", m.Dig("foo", "bar"))
 
