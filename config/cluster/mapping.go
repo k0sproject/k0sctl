d646723b0eb283ace6b34e5e02d72adf096075a1
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-02-01 11:14:10 +0200

    Improved Mapping.Dig + added Mapping.DeepSet

--- config/cluster/mapping.go
+++ config/cluster/mapping.go
@@ -19,37 +19,58 @@ func (m *Mapping) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // Dig is very naive implementation for ruby-like Hash.dig functionality
 // Returns nested value which is specified by the sequence of the key object by calling dig at each step. otherwise nil
 func (m *Mapping) Dig(keys ...string) interface{} {
-	v := (*m)[keys[0]]
-	if v == nil {
+	v, ok := (*m)[keys[0]]
+	if !ok {
 		return nil
 	}
 	switch v := v.(type) {
-	case []interface{}:
-		return v
 	case Mapping:
+		if len(keys) == 1 {
+			return v
+		}
 		return v.Dig(keys[1:]...)
-	case string:
-		return v
-	case int:
-		return v
-	case bool:
-		return v
 	default:
-		return fmt.Sprintf("%v", v)
+		if len(keys) > 1 {
+			return nil
+		}
+		return v
 	}
 }
 
 // DigString is like Dig but returns the value as string type
 func (m *Mapping) DigString(keys ...string) string {
 	v := m.Dig(keys...)
-	if v == nil {
-		return ""
-	}
 	val, ok := v.(string)
 	if !ok {
 		return ""
 	}
 	return val
+}
+
+// DeepSet sets a value deep in a nested mapping, creating new levels of mappings as it goes unless they exist
+func (m *Mapping) DeepSet(keys []string, value interface{}) {
+	k := keys[0]
+	cur := (*m)[k]
+	switch v := cur.(type) {
+	case nil:
+		if len(keys) > 1 {
+			n := Mapping{}
+			(*m)[k] = n
+			n.DeepSet(keys[1:], value)
+		} else {
+			(*m)[k] = value
+		}
+	case Mapping:
+		if len(keys) > 1 {
+			v.DeepSet(keys[1:], value)
+		} else {
+			(*m)[k] = value
+		}
+	default:
+		if len(keys) == 1 {
+			(*m)[k] = value
+		}
+	}
 }
 
 // Cleans up a slice of interfaces into slice of actual values
--- config/cluster/mapping_test.go
+++ config/cluster/mapping_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestDig(t *testing.T) {
-
 	m := Mapping{
 		"foo": Mapping{
 			"bar": "foobar",
@@ -18,5 +17,23 @@ func TestDig(t *testing.T) {
 	assert.Equal(t, "foobar", m.DigString("foo", "bar"))
 
 	assert.Nil(t, m.Dig("foo", "non-existing", "key"))
+}
 
+func TestDeepSet(t *testing.T) {
+	m := Mapping{
+		"foo": Mapping{
+			"bar": "foobar",
+		},
+	}
+
+	m.DeepSet([]string{"bar", "baz", "dog"}, 1)
+	assert.Equal(t, 1, m.Dig("bar", "baz", "dog"))
+
+	m.DeepSet([]string{"foo", "baz", "dog"}, "hello")
+	assert.Equal(t, "hello", m.Dig("foo", "baz", "dog"))
+	assert.Equal(t, "foobar", m.Dig("foo", "bar"))
+
+	m.DeepSet([]string{"foo", "bar", "baz"}, "hello")
+	assert.Nil(t, m.Dig("foo", "bar", "baz"))
+	assert.Equal(t, "foobar", m.Dig("foo", "bar"))
 }
