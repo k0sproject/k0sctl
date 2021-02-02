279be13d529cefb9fe8ea6489c852538bfb4484f
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-02-02 12:59:50 +0200

    Improve node join logic (#54)
    * Generate short-lived tokens before use
    
    * Longer token life for controllers
    
    * Add config path
    
    * More worker token longevity
    
    * Unique token per controller
    
    * It needs to create a worker token..

--- phase/install_controllers.go
+++ phase/install_controllers.go
@@ -1,6 +1,8 @@
 package phase
 
 import (
+	"time"
+
 	"github.com/k0sproject/k0sctl/config"
 	"github.com/k0sproject/k0sctl/config/cluster"
 	log "github.com/sirupsen/logrus"
@@ -9,7 +11,8 @@ import (
 // InstallControllers installs k0s controllers and joins them to the cluster
 type InstallControllers struct {
 	GenericPhase
-	hosts cluster.Hosts
+	hosts  cluster.Hosts
+	leader *cluster.Host
 }
 
 // Title for the phase
@@ -21,8 +24,10 @@ func (p *InstallControllers) Title() string {
 func (p *InstallControllers) Prepare(config *config.Cluster) error {
 	p.Config = config
 	var controllers cluster.Hosts = p.Config.Spec.Hosts.Controllers()
-	leader := p.Config.Spec.K0sLeader()
-	p.hosts = controllers.Filter(func(h *cluster.Host) bool { return h != leader })
+	p.leader = p.Config.Spec.K0sLeader()
+	p.hosts = controllers.Filter(func(h *cluster.Host) bool {
+		return h != p.leader && h.Metadata.K0sRunningVersion == ""
+	})
 
 	return nil
 }
@@ -34,32 +39,38 @@ func (p *InstallControllers) ShouldRun() bool {
 
 // Run the phase
 func (p *InstallControllers) Run() error {
-	return p.hosts.ParallelEach(func(h *cluster.Host) error {
-		if h.Metadata.K0sRunningVersion == "" {
-			log.Infof("%s: writing join token", h)
-			if err := h.Configurer.WriteFile(h, h.K0sJoinTokenPath(), p.Config.Spec.K0s.Metadata.ControllerToken, "0640"); err != nil {
-				return err
-			}
-
-			log.Infof("%s: installing k0s controller", h)
-			if err := h.Exec(h.K0sInstallCommand()); err != nil {
-				return err
-			}
-			log.Infof("%s: starting service", h)
-			if err := h.Configurer.StartService(h, h.K0sServiceName()); err != nil {
-				return err
-			}
-		} else {
-			log.Infof("%s: k0s server already running", h)
+	for _, h := range p.hosts {
+		log.Infof("%s: generating token", p.leader)
+		token, err := p.Config.Spec.K0s.GenerateToken(
+			p.leader,
+			"controller",
+			time.Duration(10)*time.Minute,
+		)
+		if err != nil {
+			return err
 		}
 
-		if NoWait {
-			log.Debugf("%s: not waiting because --no-wait given", h)
-			return nil
+		log.Infof("%s: writing join token", h)
+		if err := h.Configurer.WriteFile(h, h.K0sJoinTokenPath(), token, "0640"); err != nil {
+			return err
 		}
 
-		return p.waitJoined(h)
-	})
+		log.Infof("%s: installing k0s controller", h)
+		if err := h.Exec(h.K0sInstallCommand()); err != nil {
+			return err
+		}
+
+		log.Infof("%s: starting service", h)
+		if err := h.Configurer.StartService(h, h.K0sServiceName()); err != nil {
+			return err
+		}
+
+		if err := p.waitJoined(h); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (p *InstallControllers) waitJoined(h *cluster.Host) error {
