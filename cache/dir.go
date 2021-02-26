1fe26240f29fb7cad7eb3c31b07dc57350ea8eb2
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-02-26 12:41:53 +0200

    Fall back to ~/.k0sctl/cache for cache dir

--- cache/dir.go
+++ cache/dir.go
@@ -8,8 +8,26 @@ import (
 	"runtime"
 )
 
-// Dir returns the directory where k0sctl temporary files should be stored
+// Dir returns the directory where k0sctl temporary files should be stored. The directory will be created if it does not exist.
 func Dir() string {
+	d := defaultdir()
+	if err := EnsureDir(d); err != nil {
+		// Fall back to ~/.k0sctl/cache
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return ""
+		}
+		fb := path.Join(home, ".k0sctl", "cache")
+		if err := EnsureDir(fb); err != nil {
+			return ""
+		}
+		return fb
+	}
+
+	return d
+}
+
+func defaultdir() string {
 	switch runtime.GOOS {
 	case "linux":
 		return "/var/cache/k0sctl"
