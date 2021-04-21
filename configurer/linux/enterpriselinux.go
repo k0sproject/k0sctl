bc2350e25ac6c9e7bdde66d316723321efc0b7ad
Natanael Copa <ncopa@mirantis.com>
2021-04-21 15:18:00 +0200

    Use kubectl embedded in k0s
    Use the kubectl embedded in k0s instead of downloading from network.
    This solve issue with air-gap installs.
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- configurer/linux/enterpriselinux.go
+++ configurer/linux/enterpriselinux.go
@@ -2,7 +2,6 @@ package linux
 
 import (
 	"github.com/k0sproject/k0sctl/configurer"
-	"github.com/k0sproject/rig/os"
 	"github.com/k0sproject/rig/os/linux"
 )
 
@@ -10,22 +9,4 @@ import (
 type EnterpriseLinux struct {
 	linux.EnterpriseLinux
 	configurer.Linux
-}
-
-// InstallKubectl installs kubectl using the gcloud kubernetes repo
-func (c EnterpriseLinux) InstallKubectl(h os.Host) error {
-	err := c.WriteFile(h, "/etc/yum.repos.d/kubernetes.repo", `[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-`, "0644")
-
-	if err != nil {
-		return err
-	}
-
-	return c.InstallPackage(h, "kubectl")
 }
