12ed1175427d88af119422ccc6a0860b99ebc2ce
Jussi Nummelin <jnummelin@mirantis.com>
2021-04-21 20:55:53 +0000

    I see dead code
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- configurer/linux/alpine.go
+++ configurer/linux/alpine.go
@@ -14,13 +14,6 @@ type BaseLinux struct {
 	configurer.Linux
 }
 
-var kubectlInstallScript = []string{
-	`curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"`,
-	`curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"`,
-	`echo "$(<kubectl.sha256) kubectl" | sha256sum --check`,
-	`sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl`,
-}
-
 // Alpine provides OS support for Alpine Linux
 type Alpine struct {
 	os.Linux
