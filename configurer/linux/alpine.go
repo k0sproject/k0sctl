04f6ac4d97a507a31eeb34df1f7ff0a53a633604
Jussi Nummelin <jnummelin@users.noreply.github.com>
2021-04-23 11:54:15 +0300

    Replace kubectl usage with embedded k0s kubectl (#127)
    * Replace installed kubectl usage with k0s embedded kubectl usage
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    
    * Use sudo with install when creating the upload target dir
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    
    * Add note to bump up rig version
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    
    * I see dead code
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    
    * Move k0s upload/download phases up so we get proper support for embedded kubectl command for upgrade case
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    
    * Bump rig to 0.3.17 to get fixes for file uploads
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- configurer/linux/alpine.go
+++ configurer/linux/alpine.go
@@ -14,23 +14,6 @@ type BaseLinux struct {
 	configurer.Linux
 }
 
-var kubectlInstallScript = []string{
-	`curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"`,
-	`curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"`,
-	`echo "$(<kubectl.sha256) kubectl" | sha256sum --check`,
-	`sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl`,
-}
-
-// InstallKubectl installs kubectl using the curl method
-func (l BaseLinux) InstallKubectl(h os.Host) error {
-	for _, c := range kubectlInstallScript {
-		if err := h.Exec(c); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // Alpine provides OS support for Alpine Linux
 type Alpine struct {
 	os.Linux
@@ -51,11 +34,6 @@ func init() {
 // InstallPackage installs packages via slackpkg
 func (l Alpine) InstallPackage(h os.Host, pkg ...string) error {
 	return h.Execf("sudo apk add --update %s", strings.Join(pkg, " "))
-}
-
-// InstallKubectl installs kubectl using the alpine edge/testing repo
-func (l Alpine) InstallKubectl(h os.Host) error {
-	return l.InstallPackage(h, "--repository https://dl-cdn.alpinelinux.org/alpine/edge/testing kubectl")
 }
 
 func (l Alpine) Prepare(h os.Host) error {
