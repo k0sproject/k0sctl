d933c5809020539dadcb1cfdc28418f8564bd368
Jussi Nummelin <jnummelin@mirantis.com>
2021-06-04 13:22:39 +0000

    Add backup as part of the basic smoke
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- smoke-test/smoke-basic.sh
+++ smoke-test/smoke-basic.sh
@@ -11,3 +11,6 @@ deleteCluster
 createCluster
 ../k0sctl apply --config ${K0SCTL_YAML} --debug
 ../k0sctl kubeconfig --config ${K0SCTL_YAML} | grep -v -- "-data"
+# just to check we can get the backup file succesfully.
+# Once we have restore capability on k0sctl we might want to have the full apply&backup&reset&restore process as separate smoke
+../k0sctl backup --config ${K0SCTL_YAML}
