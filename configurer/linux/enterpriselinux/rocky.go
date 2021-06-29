e735b4121df7021b11233f7818889dc39e721c88
Christian Rau <48926486+Christian-Rau@users.noreply.github.com>
2021-06-29 13:39:57 +0200

    Added support for additional distro´s
    When running phase runs: AlmaLinux & Rocky Linux are now being detected.
    
    Signed-off-by: Christian Rau <48926486+Christian-Rau@users.noreply.github.com>

---
+++ configurer/linux/enterpriselinux/rocky.go
@@ -0,0 +1,25 @@
+package enterpriselinux
+
+import (
+	"github.com/k0sproject/k0sctl/configurer"
+	k0slinux "github.com/k0sproject/k0sctl/configurer/linux"
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// RockyLinux provides OS support for RockyLinux
+type RockyLinux struct {
+	k0slinux.EnterpriseLinux
+	configurer.Linux
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "rocky"
+		},
+		func() interface{} {
+			return RockyLinux{}
+		},
+	)
+}
