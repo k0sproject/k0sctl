e9016a48e507922c9e68f6da2fe329d26c870234
Christian Rau <48926486+Christian-Rau@users.noreply.github.com>
2021-07-26 12:40:14 +0200

    Add support for Alma Linux and Rocky Linux (#165)
    When running phase runs: AlmaLinux & Rocky Linux are now being detected.
    
    Signed-off-by: Christian Rau <48926486+Christian-Rau@users.noreply.github.com>

---
+++ configurer/linux/enterpriselinux/almalinux.go
@@ -0,0 +1,25 @@
+package enterpriselinux
+
+import (
+	"github.com/k0sproject/k0sctl/configurer"
+	k0slinux "github.com/k0sproject/k0sctl/configurer/linux"
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// AlmaLinux provides OS support for AlmaLinux
+type AlmaLinux struct {
+	k0slinux.EnterpriseLinux
+	configurer.Linux
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "almalinux"
+		},
+		func() interface{} {
+			return AlmaLinux{}
+		},
+	)
+}
