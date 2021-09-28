efd6540beb7e011d8e0f9a565e0dd53fdc8d20fe
Elias Werberich <elias@werberich.de>
2021-09-28 12:54:59 +0000

    Add support for FlatcarContainerLinux

---
+++ configurer/linux/flatcar.go
@@ -0,0 +1,36 @@
+package linux
+
+import (
+	"errors"
+
+	"github.com/k0sproject/k0sctl/configurer"
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/os"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+type Flatcar struct {
+	BaseLinux
+	os.Linux
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "flatcar"
+		},
+		func() interface{} {
+			linuxType := &Flatcar{}
+			linuxType.LinuxStaticConstants = interface{}(linuxType).(configurer.LinuxStaticConstants)
+			return linuxType
+		},
+	)
+}
+
+func (l Flatcar) InstallPackage(h os.Host, pkg ...string) error {
+	return errors.New("FlatcarContainerLinux does not support installing packages manually")
+}
+
+func (l Flatcar) K0sBinaryPath() string {
+	return "/opt/bin/k0s"
+}
