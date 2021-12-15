ac7307290ff9210dedf9de1fba9f58aeea303a3d
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-12-15 10:56:15 +0200

    Fix nil panic when config has no spec.k0s (#291)
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster/spec.go
+++ pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster/spec.go
@@ -19,6 +19,7 @@ type Spec struct {
 func (s *Spec) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	type spec Spec
 	ys := (*spec)(s)
+	ys.K0s = &K0s{}
 
 	if err := unmarshal(ys); err != nil {
 		return err
