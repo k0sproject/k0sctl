de29f46e356ad2d11fc6a8935aab6ff51c670d6b
Cedric Lewe <0skillallluck@pm.me>
2022-09-27 15:58:19 +0200

    Use mockHost for TestPaths

--- configurer/linux/linux_test.go
+++ configurer/linux/linux_test.go
@@ -1,11 +1,48 @@
 package linux
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/k0sproject/k0sctl/configurer"
+	"github.com/k0sproject/rig/exec"
 	"github.com/stretchr/testify/require"
 )
+
+type mockHost struct {
+	ExecError bool
+}
+
+func (m mockHost) Upload(source, destination string, opts ...exec.Option) error {
+	return nil
+}
+
+func (m mockHost) Exec(string, ...exec.Option) error {
+	if m.ExecError {
+		return fmt.Errorf("error")
+	}
+	return nil
+}
+
+func (m mockHost) ExecOutput(string, ...exec.Option) (string, error) {
+	return "", nil
+}
+
+func (m mockHost) Execf(string, ...interface{}) error {
+	return nil
+}
+
+func (m mockHost) ExecOutputf(string, ...interface{}) (string, error) {
+	return "", nil
+}
+
+func (m mockHost) String() string {
+	return ""
+}
+
+func (m mockHost) Sudo(string) (string, error) {
+	return "", nil
+}
 
 // TestPaths tests the slightly weird way to perform function overloading
 func TestPaths(t *testing.T) {
@@ -15,15 +52,22 @@ func TestPaths(t *testing.T) {
 	ubuntu := &Ubuntu{}
 	ubuntu.PathFuncs = interface{}(ubuntu).(configurer.PathFuncs)
 
+	h1 := &mockHost{
+		ExecError: false,
+	}
+	h2 := &mockHost{
+		ExecError: true,
+	}
+
 	require.Equal(t, "/opt/bin/k0s", fc.K0sBinaryPath())
 	require.Equal(t, "/usr/local/bin/k0s", ubuntu.K0sBinaryPath())
 
 	require.Equal(t, "/opt/bin/k0s --help", fc.K0sCmdf("--help"))
 	require.Equal(t, "/usr/local/bin/k0s --help", ubuntu.K0sCmdf("--help"))
 
-	require.Equal(t, "/var/lib/k0s/pki/admin.conf", fc.KubeconfigPath(true))
-	require.Equal(t, "/var/lib/k0s/pki/admin.conf", ubuntu.KubeconfigPath(true))
+	require.Equal(t, "/var/lib/k0s/pki/admin.conf", fc.KubeconfigPath(h1))
+	require.Equal(t, "/var/lib/k0s/pki/admin.conf", ubuntu.KubeconfigPath(h1))
 
-	require.Equal(t, "/var/lib/k0s/kubelet.conf", fc.KubeconfigPath(false))
-	require.Equal(t, "/var/lib/k0s/kubelet.conf", ubuntu.KubeconfigPath(false))
+	require.Equal(t, "/var/lib/k0s/kubelet.conf", fc.KubeconfigPath(h2))
+	require.Equal(t, "/var/lib/k0s/kubelet.conf", ubuntu.KubeconfigPath(h2))
 }
