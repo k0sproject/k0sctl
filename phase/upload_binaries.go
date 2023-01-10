4f7c2e1dbb593a0de9e3e7f4c4620f80f28ac5cc
Kimmo Lehto <klehto@mirantis.com>
2023-01-10 11:43:49 +0200

    Unify the way k0s binary is installed after upload or download
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- phase/upload_binaries.go
+++ phase/upload_binaries.go
@@ -3,12 +3,9 @@ package phase
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/k0sproject/k0sctl/pkg/apis/k0sctl.k0sproject.io/v1beta1"
 	"github.com/k0sproject/k0sctl/pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster"
-	"github.com/k0sproject/rig/exec"
-	"github.com/k0sproject/version"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -37,13 +34,12 @@ func (p *UploadBinaries) Prepare(config *v1beta1.Cluster) error {
 			return false
 		}
 
-		// Upgrade is handled separately (k0s stopped, binary uploaded, k0s restarted)
-		if h.Metadata.NeedsUpgrade {
+		// The version is already correct
+		if h.Metadata.K0sBinaryVersion == p.Config.Spec.K0s.Version {
 			return false
 		}
 
-		// The version is already correct
-		if h.Metadata.K0sBinaryVersion == p.Config.Spec.K0s.Version {
+		if !h.FileChanged(h.UploadBinaryPath, h.Configurer.K0sBinaryPath()) {
 			return false
 		}
 
@@ -62,58 +58,27 @@ func (p *UploadBinaries) Run() error {
 	return p.parallelDoUpload(p.hosts, p.uploadBinary)
 }
 
-func (p *UploadBinaries) ensureBinPath(h *cluster.Host) error {
-	dir := filepath.Dir(h.Configurer.K0sBinaryPath())
-	// FileExist uses "-e" which also works for dirs
-	if h.Configurer.FileExist(h, dir) {
-		return nil
-	}
-	if err := h.Configurer.MkDir(h, dir, exec.Sudo(h)); err != nil {
-		return fmt.Errorf("failed to create %s: %w", dir, err)
-	}
-	if err := h.Configurer.Chmod(h, dir, "0755", exec.Sudo(h)); err != nil {
-		return fmt.Errorf("failed to chmod %s: %w", dir, err)
-	}
-	return nil
-}
-
 func (p *UploadBinaries) uploadBinary(h *cluster.Host) error {
+	tmp, err := h.Configurer.TempFile(h)
+	if err != nil {
+		return fmt.Errorf("failed to create tempfile %w", err)
+	}
+
 	stat, err := os.Stat(h.UploadBinaryPath)
 	if err != nil {
-		return fmt.Errorf("failed to stat %s: %w", h.UploadBinaryPath, err)
-	}
-	if h.FileChanged(h.UploadBinaryPath, h.Configurer.K0sBinaryPath()) {
-		if err := p.ensureBinPath(h); err != nil {
-			return err
-		}
-		log.Infof("%s: uploading k0s binary from %s", h, h.UploadBinaryPath)
-		if err := h.Upload(h.UploadBinaryPath, h.Configurer.K0sBinaryPath(), exec.Sudo(h)); err != nil {
-			return err
-		}
-	} else {
-		log.Infof("%s: k0s binary %s already exists on the target and hasn't been changed, skipping upload", h, h.UploadBinaryPath)
+		return fmt.Errorf("stat %s: %w", h.UploadBinaryPath, err)
 	}
 
-	if err := h.Configurer.Chmod(h, h.Configurer.K0sBinaryPath(), "0700", exec.Sudo(h)); err != nil {
-		return err
+	log.Infof("%s: uploading k0s binary from %s", h, h.UploadBinaryPath)
+	if err := h.Upload(h.UploadBinaryPath, tmp); err != nil {
+		return fmt.Errorf("upload k0s binary: %w", err)
 	}
 
-	log.Debugf("%s: touching %s", h, h.Configurer.K0sBinaryPath())
-	if err := h.Configurer.Touch(h, h.Configurer.K0sBinaryPath(), stat.ModTime(), exec.Sudo(h)); err != nil {
-		return fmt.Errorf("failed to touch %s: %w", h.Configurer.K0sBinaryPath(), err)
+	if err := h.Configurer.Touch(h, tmp, stat.ModTime()); err != nil {
+		return fmt.Errorf("failed to touch %s: %w", tmp, err)
 	}
 
-	uploadedVersion, err := h.Configurer.K0sBinaryVersion(h)
-	if err != nil {
-		return fmt.Errorf("failed to get uploaded k0s binary version: %w", err)
-	}
-
-	h.Metadata.K0sBinaryVersion = uploadedVersion.String()
-	log.Debugf("%s: has k0s binary version %s", h, h.Metadata.K0sBinaryVersion)
-
-	if version, err := version.NewVersion(p.Config.Spec.K0s.Version); err == nil && !version.Equal(uploadedVersion) {
-		return fmt.Errorf("uploaded k0s binary version is %s not %s", uploadedVersion, version)
-	}
+	h.Metadata.K0sBinaryTempFile = tmp
 
 	return nil
 }
