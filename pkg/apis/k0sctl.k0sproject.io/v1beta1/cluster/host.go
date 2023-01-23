48a2a4603170390b0d926eae14f0c7e5ac071482
Ethan Mosbaugh <ethan@replicated.com>
2023-01-23 21:31:20 +0000

    fix: localhost panic nil pointer dereference

--- pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster/host.go
+++ pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster/host.go
@@ -166,7 +166,7 @@ func (h *Host) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return err
 	}
 
-	if h.SSH.HostKey != "" {
+	if h.SSH != nil && h.SSH.HostKey != "" {
 		log.Warnf("%s: host.ssh.hostKey is deprecated, use a ssh known hosts file instead", h)
 	}
 
