b5437f62375ea2af5a2ee6effdbf9f9e17e954b7
Kimmo Lehto <klehto@mirantis.com>
2023-01-23 16:04:19 +0200

    Validate host private address uniqueness (#453)
    * Validate host private address uniqueness
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- phase/validate_hosts.go
+++ phase/validate_hosts.go
@@ -9,8 +9,9 @@ import (
 // ValidateHosts performs remote OS detection
 type ValidateHosts struct {
 	GenericPhase
-	hncount        map[string]int
-	machineidcount map[string]int
+	hncount          map[string]int
+	machineidcount   map[string]int
+	privateaddrcount map[string]int
 }
 
 // Title for the phase
@@ -22,17 +23,34 @@ func (p *ValidateHosts) Title() string {
 func (p *ValidateHosts) Run() error {
 	p.hncount = make(map[string]int, len(p.Config.Spec.Hosts))
 	p.machineidcount = make(map[string]int, len(p.Config.Spec.Hosts))
+	p.privateaddrcount = make(map[string]int, len(p.Config.Spec.Hosts))
+
 	for _, h := range p.Config.Spec.Hosts {
 		p.hncount[h.Metadata.Hostname]++
 		p.machineidcount[h.Metadata.MachineID]++
+		p.privateaddrcount[h.PrivateAddress]++
 	}
 
-	return p.parallelDo(p.Config.Spec.Hosts, p.validateUniqueHostname, p.validateUniqueMachineID, p.validateSudo)
+	return p.parallelDo(
+		p.Config.Spec.Hosts,
+		p.validateUniqueHostname,
+		p.validateUniqueMachineID,
+		p.validateUniquePrivateAddress,
+		p.validateSudo,
+	)
 }
 
 func (p *ValidateHosts) validateUniqueHostname(h *cluster.Host) error {
 	if p.hncount[h.Metadata.Hostname] > 1 {
 		return fmt.Errorf("hostname is not unique: %s", h.Metadata.Hostname)
+	}
+
+	return nil
+}
+
+func (p *ValidateHosts) validateUniquePrivateAddress(h *cluster.Host) error {
+	if p.privateaddrcount[h.PrivateAddress] > 1 {
+		return fmt.Errorf("privateAddress %s is not unique: %s", h.PrivateAddress, h.Metadata.Hostname)
 	}
 
 	return nil
