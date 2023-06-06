k0sproject/k0sctl

commit 0280b8bb6165c6e575ee6e0caea6478e6e0316d7

Validate host private address uniqueness
Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

phase/validate_hosts.go
@@ -9,8 +9,9 @@ import (
 // ValidateHosts performs remote OS detection
 type ValidateHosts struct {
 	GenericPhase
-	hncount        map[string]int
-	machineidcount map[string]int
+	hncount          map[string]int
+	machineidcount   map[string]int
+	privateaddrcount map[string]int
 }
 
 // Title for the phase
@@ -25,6 +26,7 @@ func (p *ValidateHosts) Run() error {
 	for _, h := range p.Config.Spec.Hosts {
 		p.hncount[h.Metadata.Hostname]++
 		p.machineidcount[h.Metadata.MachineID]++
+		p.privateaddrcount[h.PrivateAddress]++
 	}
 
 	return p.parallelDo(p.Config.Spec.Hosts, p.validateUniqueHostname, p.validateUniqueMachineID, p.validateSudo)
@@ -33,6 +35,14 @@ func (p *ValidateHosts) Run() error {
 func (p *ValidateHosts) validateUniqueHostname(h *cluster.Host) error {
 	if p.hncount[h.Metadata.Hostname] > 1 {
 		return fmt.Errorf("hostname is not unique: %s", h.Metadata.Hostname)
+	}
+
+	return nil
+}
+
+func (p *ValidateHosts) validateUniquePrivateAddress(h *cluster.Host) error {
+	if p.privateaddrcount[h.PrivateAddress] > 1 {
+		return fmt.Errorf("privateAddress %s is not unique: %s", h.PrivateAddress, h.Metadata.Hostname)
 	}
 
 	return nil

commit 7959d6e48fc64237734e74f90c016f038dcb7816

Don't count empty private addresses during validation
This would fail as soon as there are more than one hosts without a
private address.

Fixes: 0280b8b ("Validate host private address uniqueness")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

phase/validate_hosts.go
@@ -28,7 +28,9 @@ func (p *ValidateHosts) Run() error {
 	for _, h := range p.Config.Spec.Hosts {
 		p.hncount[h.Metadata.Hostname]++
 		p.machineidcount[h.Metadata.MachineID]++
-		p.privateaddrcount[h.PrivateAddress]++
+		if h.PrivateAddress != "" {
+			p.privateaddrcount[h.PrivateAddress]++
+		}
 	}
 
 	return p.parallelDo(
@@ -50,7 +52,7 @@ func (p *ValidateHosts) validateUniqueHostname(h *cluster.Host) error {
 
 func (p *ValidateHosts) validateUniquePrivateAddress(h *cluster.Host) error {
 	if p.privateaddrcount[h.PrivateAddress] > 1 {
-		return fmt.Errorf("privateAddress %s is not unique: %s", h.PrivateAddress, h.Metadata.Hostname)
+		return fmt.Errorf("privateAddress %q is not unique: %s", h.PrivateAddress, h.Metadata.Hostname)
 	}
 
 	return nil