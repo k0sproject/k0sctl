e7a1215400ae5f96aa150077a7ca73e0fa6137f6
Kimmo Lehto <klehto@mirantis.com>
2024-04-26 10:37:29 +0300

    Remove panic handling that was used to send backtraces in telemetry
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"os"
-	"runtime"
-	"strings"
 
 	"github.com/k0sproject/k0sctl/cmd"
 	log "github.com/sirupsen/logrus"
@@ -14,31 +12,7 @@ import (
 	_ "github.com/k0sproject/k0sctl/version"
 )
 
-func handlepanic() {
-	if err := recover(); err != nil {
-		buf := make([]byte, 1<<16)
-		ss := runtime.Stack(buf, true)
-		msg := string(buf[:ss])
-		var bt []string
-		for _, row := range strings.Split(msg, "\n") {
-			if !strings.HasPrefix(row, "\t") {
-				continue
-			}
-			if strings.Contains(row, "main.") {
-				continue
-			}
-			if strings.Contains(row, "panic") {
-				continue
-			}
-			bt = append(bt, strings.TrimSpace(row))
-		}
-
-		log.Fatalf("PANIC: %v\n", err)
-	}
-}
-
 func main() {
-	defer handlepanic()
 	err := cmd.App.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
