5e44415787f14d362ea0f2a10816bc197f6bd4f6
Kimmo Lehto <klehto@mirantis.com>
2025-02-19 14:51:39 +0200

    Try to exit gracefully and run clean up on interrupt (#833)
    * Initialize and propagate a global context with timeout
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Propagate context deeper into Each/ParallelEach
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Abort Each/ParallelEach early on context errors
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Introduce retry.AdaptiveTimeout and deepen propagation
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Abort phase manager on dead context
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Use context timeout for joined check
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    * Attempt a graceful exit on interrupt
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    
    ---------
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,26 @@
 package cmd
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
+
+func trapSignals(ctx context.Context, cancel context.CancelFunc) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ch:
+		log.Warnf("received an interrupt signal, aborting operation")
+		cancel()
+	case <-ctx.Done():
+		return
+	}
+}
 
 // App is the main urfave/cli.App for k0sctl
 var App = &cli.App{
@@ -31,4 +49,16 @@ var App = &cli.App{
 		completionCommand,
 	},
 	EnableBashCompletion: true,
+	Before: func(ctx *cli.Context) error {
+		if globalCancel == nil {
+			cancelCtx, cancel := context.WithCancel(ctx.Context)
+			ctx.Context = cancelCtx
+			globalCancel = cancel
+		}
+		go trapSignals(ctx.Context, globalCancel)
+		return nil
+	},
+	After: func(ctx *cli.Context) error {
+		return cancelTimeout(ctx)
+	},
 }
