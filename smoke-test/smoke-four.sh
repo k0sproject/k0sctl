7db7d8a470d90ff2a1cd35a8ec32bc00e1b0b162
Kimmo Lehto <klehto@mirantis.com>
2025-05-12 16:36:36 +0300

    (tmp) try running on four controllers
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

---
+++ smoke-test/smoke-four.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+K0SCTL_CONFIG=${K0SCTL_CONFIG:-"k0sctl-four.yaml"}
+
+set -e
+
+
+. ./smoke.common.sh
+trap cleanup EXIT
+
+deleteCluster
+createCluster
+
+echo "* Starting apply"
+../k0sctl apply --config "${K0SCTL_CONFIG}" --kubeconfig-out applykubeconfig --debug
+echo "* Apply OK"
+
+echo "* Verify 'k0sctl kubeconfig' output includes 'data' block"
+../k0sctl kubeconfig --config k0sctl.yaml | grep -v -- "-data"
+
+echo "* Run kubectl on controller"
+bootloose ssh root@manager0 -- k0s kubectl get nodes
+
+echo "* Downloading kubectl for local test"
+downloadKubectl
+
+echo "* Using the kubectl from apply"
+./kubectl --kubeconfig applykubeconfig get nodes
+
+echo "* Using k0sctl kubecofig locally"
+../k0sctl kubeconfig --config k0sctl.yaml --user smoke --cluster test > kubeconfig
+
+echo "* Output:"
+grep -v -- -data kubeconfig
+
+echo "* Running kubectl"
+./kubectl --kubeconfig kubeconfig --user smoke --cluster test get nodes
+echo "* Done"
+
