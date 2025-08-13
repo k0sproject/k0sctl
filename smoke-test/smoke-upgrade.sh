9b44b1fd0c78658e281d39af1fb8b88f8a7e9851
Kimmo Lehto <klehto@mirantis.com>
2025-08-13 12:40:07 +0300

    Force in smoke
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- smoke-test/smoke-upgrade.sh
+++ smoke-test/smoke-upgrade.sh
@@ -28,5 +28,12 @@ K0S_VERSION=$(curl -s "https://docs.k0sproject.io/stable.txt")
 
 # Create config with latest version and apply as upgrade
 echo "Upgrading to k0s ${K0S_VERSION}"
-../k0sctl apply --config "${K0SCTL_CONFIG}" --debug
+# First attempt should fail without --force because of version skew (tee stdout/stderr to log file)
+if ../k0sctl apply --config "${K0SCTL_CONFIG}" --debug 2>&1 | tee apply.log | grep -q "not within kubernetes version skew policy"; then
+  echo "Expected skew failure when applying without --force"
+  exit 1
+fi
+
+# Second attempt should succeed with --force
+../k0sctl apply --config "${K0SCTL_CONFIG}" --debug --force
 remoteCommand "root@manager0" "k0s version | grep -q ${K0S_VERSION}"
